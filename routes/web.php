5164a7d1cead1171c68db9000d5d1168a7053d54
junghwan.yang <junghwan.yagn@adaware.com>
2017-09-15 15:16:24 -0400

    default route setting, welcome controller, auth middleware set to welcome controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,60 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    'as' => 'root',
+    'uses' => 'WelcomeController@index'
+]);
+
+Route::get('home', [
+    'as' => 'home',
+    'uses' => 'WelcomeController@home'
+]);
+
+// User Registration
+Route::group(['prefix' => 'auth', 'as' => 'user.'], function() {
+    Route::get('register', [
+        'as' => 'create',
+        'uses' => 'Auth\RegisterController@showRegistrationForm'
+    ]);
+    Route::get('register', [
+        'as' => 'store',
+        'uses' => 'Auth\RegisterController@register'
+    ]);
+});
+
+// Session
+Route::group(['prefix' => 'auth', 'as' => 'session.'], function() {
+    Route::get('login', [
+        'as' => 'create',
+        'uses' => 'Auth\LoginController@showLoginForm'
+    ]);
+    Route::get('login', [
+        'as' => 'store',
+        'uses' => 'Auth\LoginController@login'
+    ]);
+    Route::get('login', [
+        'as' => 'destroy',
+        'uses' => 'Auth\LoginController@logout'
+    ]);
+});
+
+// Password Reminder
+Route::group(['prefix' => 'password'], function() {
+    Route::get('remind', [
+        'as' => 'reminder.create',
+        'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'
+    ]);
+    Route::get('remind', [
+        'as' => 'reminder.store',
+        'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'
+    ]);
+    Route::get('reset/{token}', [
+        'as' => 'reset.create',
+        'uses' => 'Auth\ResetPasswordController@showResetForm'
+    ]);
+    Route::get('reset', [
+        'as' => 'reset.store',
+        'uses' => 'Auth\ResetPasswordController@reset'
+    ]);
 });
